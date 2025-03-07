8eb69d96ecfb96f0c6fa514a13c870060e2e6616
Parthy Chandrasekaran <partheinstein@gmail.com>
2025-03-07 02:07:56 +0000

    Read serial

--- cup.sh
+++ cup.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 arduino-cli compile --fqbn arduino:avr:uno $1
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno $1
+arduino-cli upload -p /dev/ttyACM1 --fqbn arduino:avr:uno $1
